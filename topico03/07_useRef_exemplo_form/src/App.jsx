bf19237a12dd069523ac7832c9563eda66820e11
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-03-23 23:51:22 -0300

    feat: exemplos com forms e os hooks useState, useEffect e useRef

---
+++ topico03/07_useRef_exemplo_form/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import './App.css';
+import CardImc from './components/CardImc/CardImcFit';
+import FormCadastraPessoa from './components/FormCadastraPessoaState';
+
+function App() {
+
+  const [pessoa, setPessoa] = useState(null)
+
+  return (
+    <>
+      {!pessoa && <FormCadastraPessoa setPessoa={setPessoa} disabledButton={pessoa}/>}
+      {/* {!pessoa && <button onClick={cadastraPessoa}>Cadastrar</button>} */}
+     {pessoa
+        ? <><CardImc key="p3" pessoa={pessoa} /></>
+        : <p>Cadastre uma Pessoa</p>
+      }
+    </>
+  );
+}
+
+export default App;
