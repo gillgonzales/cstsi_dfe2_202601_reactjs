0c843bb2fc887931ee354bd745f69ed23860efae
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-05-11 15:21:46 -0300

    feat: uso da PROPS com styled-components. Passagem de propriedades para componentes estilizados.

---
+++ topico07/08-react-styled-component-props/src/components/DevIllustration/DevIllustration.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { DevIllustrationStyled } from "./DevIllustration.styles";
+
+export default function DevIllustration({ isThemeLight }) {
+  
+  console.log({isThemeLight})
+  return (
+    <DevIllustrationStyled $isThemeLight={isThemeLight}/>
+  );
+}
+
+
+
