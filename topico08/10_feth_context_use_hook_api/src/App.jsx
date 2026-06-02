36ae6fbad92b81defcfd6bd9adb10741fb8a4e8c
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-06-02 17:37:37 -0300

    refactor: utiliza o provider de produtos direto na raiz do app

--- topico08/10_feth_context_use_hook_api/src/App.jsx
+++ topico08/10_feth_context_use_hook_api/src/App.jsx
@@ -1,8 +1,12 @@
 import { AppRoutes } from './routes/AppRoutes'
 import './styles/Main.css'
 
+import ProdutosProvider from "./context/ProdutosProvider.jsx"
+
 function App() {
-  return <AppRoutes/>
+  return <ProdutosProvider>
+    <AppRoutes/>
+  </ProdutosProvider>
 }
 
 export default App
--- topico08/10_feth_context_use_hook_api/src/layouts/Guest.jsx
+++ topico08/10_feth_context_use_hook_api/src/layouts/Guest.jsx
@@ -1,7 +1,6 @@
 
 import Header from "../components/Header"
 import { Navbar } from "../components/Navbar"
-import ProdutosProvider from "../context/ProdutosProvider.jsx"
 import Home from "../pages/home"
 import {Outlet} from "react-router"
 
@@ -11,9 +10,7 @@ function Guest() {
     <Navbar/>
     <Header/>
     <main className='flex w-full justify-center p-3 sm:p-10 '>
-    <ProdutosProvider>
        <Outlet/>
-    </ProdutosProvider> 
     </main>
     </>
   )
