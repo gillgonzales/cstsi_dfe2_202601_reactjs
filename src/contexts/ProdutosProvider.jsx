223ba92f545b0e5d4fdbf1fcdf44d5d96637f8af
Prof. Gonzales <gillgonzales@ifsul.edu.br>
2026-06-24 10:49:15 -0300

    fix: endpoint token produtos

--- src/contexts/ProdutosProvider.jsx
+++ src/contexts/ProdutosProvider.jsx
@@ -36,7 +36,7 @@ const ProdutosProvider = ({ children }) => {
       formDataProduto['fornecedor_id'] = 1;
       console.log(`Cadastrar novo produto:`, formDataProduto);
 
-      const { data } = await axiosClient.post(`/tokens/produtos/`, formDataProduto, {
+      const { data } = await axiosClient.post(`/token/produtos/`, formDataProduto, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
